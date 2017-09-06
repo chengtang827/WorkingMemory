0a9ef0ffaea448ed2ab66ce42dd709fa704513e3
roger <roger.herikstad@gmail.com>
2017-09-06 13:43:56 +0800

    Fixes plus function.

--- @eyetrials/plus.m
+++ @eyetrials/plus.m
@@ -38,12 +38,10 @@ else
 		% useful fields for most objects
 		r.data.numSets = p.data.numSets + q.data.numSets;
 
-		
+
 		% object specific fields
 		r.data.dlist = [p.data.dlist; q.data.dlist];
-		r.data.setIndex = [p.data.setIndex; (p.data.setIndex(end) ...
-			+ q.data.setIndex(2:end))];
-			
+		r.data.trials = [p.data.trials q.data.trials];
 		% add nptdata objects as well
 		r.nptdata = plus(p.nptdata,q.nptdata);
 	end

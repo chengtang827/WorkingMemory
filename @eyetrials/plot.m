d41280afceb18d35e1ee7b3981fcb7ab7538708c
roger <roger.herikstad@gmail.com>
2017-09-07 09:49:51 +0800

    Adds code to plot the combined eyetraces for all trials in a session.

--- @eyetrials/plot.m
+++ @eyetrials/plot.m
@@ -15,7 +15,7 @@ if Args.ArgsOnly
     return;
 end
 
-if(~isempty(Args.NumericArguments))
+if ~isempty(Args.NumericArguments)
 	% plot one data set at a time
 	n = Args.NumericArguments{1};
 	tidx = find(obj.data.setIndex == n);
@@ -29,16 +29,25 @@ end
 
 ntrials = length(tidx);
 ff = fieldnames(obj.data.trials);
+fidx = [];
+%get rid of eye fields since we don't want to plot those here
+for fi=1:length(ff)
+	if strcmpi(ff{fi},'gazex') | strcmpi(ff{fi},'gazey') | strcmpi(ff{fi}, 'pupil')
+		continue
+	else
+		fidx = [fidx fi];
+	end
+end
+
 %create perceptually distinguisable colors for plotting
 plot_colors = distinguishable_colors(length(ff));
-ydata = nan(ntrials,length(ff));
-
+ydata = nan(ntrials,length(fidx));
 for t = 1:ntrials
 	t0 = obj.data.trials(tidx(t)).start;
 	ydata(t,1) = double(t0)/1000.0;
-	for fi=1:length(ff)
-		if ~isempty(obj.data.trials(tidx(t)).(ff{fi}))
-			xx = obj.data.trials(tidx(t)).(ff{fi});
+	for fi=1:length(fidx)
+		if ~isempty(obj.data.trials(tidx(t)).(ff{fidx(fi)}))
+			xx = obj.data.trials(tidx(t)).(ff{fidx(fi)});
 			if isstruct(xx)
 				ydata(t,fi) = double(xx.timestamp-t0)/1000.0;
 			else
@@ -48,21 +57,35 @@ for t = 1:ntrials
 	end
 end
 marker_size = 10.0;
+ax1 = subplot(1,2,1);
+ax2 = subplot(1,2,2);
 y = 1:ntrials;
-cla
-hold on
-for fi=1:length(ff)
-	plot(ydata(:,fi), y, '.', 'color', plot_colors(fi,:),...
+cla(ax1)
+hold(ax1,'on')
+for fi=1:length(fidx)
+	plot(ax1,ydata(:,fidx(fi)), y, '.', 'color', plot_colors(fi,:),...
 				'markersize', marker_size);
 end
-legend(ff)
-hold off
+%reformat legends
+legends = {};
+for fi=1:length(fidx)
+	legends{fi} = strrep(ff{fidx(fi)},'_','');
+end
+legend(ax1, legends)
+hold(ax1, 'off')
 
 % @eyetrials/PLOT takes 'LabelsOff' as an example
 if(~Args.LabelsOff)
-	xlabel('Time [s]')
-	ylabel('Trial')
+	xlabel(ax1, 'Time [s]')
+	ylabel(ax1, 'Trial')
 end
+
+hold(ax2, 'on')
+for t=1:ntrials
+	plot(ax2, obj.data.trials(tidx(t)).gazex, obj.data.trials(tidx(t)).gazey,...
+			'color',[0.0,0.749,1.0]);
+end
+hold(ax2, 'off')
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 

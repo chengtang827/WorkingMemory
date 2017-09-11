f6338e3602e227301efa2067b3f0e1494b100a7d
roger <roger.herikstad@gmail.com>
2017-09-11 14:44:30 +0800

    Adds code to return event timings from an eyetrials object.

--- @eyetrials/get.m
+++ @eyetrials/get.m
@@ -9,7 +9,8 @@ function [r,varargout] = get(obj,varargin)
 %
 %   Dependencies:
 
-Args = struct('ObjectLevel',0, 'AnalysisLevel',0, 'TrialLevel',0);
+Args = struct('ObjectLevel',0, 'AnalysisLevel',0, 'TrialLevel',0,...
+ 							'EventTiming','');
 Args.flags ={'ObjectLevel','AnalysisLevel'};
 Args = getOptArgs(varargin,Args);
 
@@ -25,6 +26,46 @@ elseif(Args.AnalysisLevel)
 elseif(Args.TrialLevel)
 	%total number of trials
 	r = length(obj.data.setIndex);
+elseif ~isempty(Args.EventTiming)
+  ts = zeros(length(obj.data.trials),1);
+  if isfield(obj.data.trials(1),Args.EventTiming)
+		if strcmpi(Args.EventTiming,'saccade')
+			%find the saccade immediately preceeding either reward or failure
+      for t = 1:length(obj.data.trials)
+        if ~isempty(obj.data.trials(t).(Args.EventTiming))
+					q = obj.data.trials(t).(Args.EventTiming);
+					if ~isnan(obj.data.trials(t).failure)
+						tf = obj.data.trials(t).failure;
+					else
+						tf = obj.data.trials(t).reward;
+					end
+					i = 1;
+					while (i < length(q)) && (q(i).onset < tf)
+						i = i + 1;
+					end
+					if i == 1
+						ts(t) = nan;
+					else
+						i = i -1;
+	          ts(t) = q(i).onset;
+					end
+        end
+			end
+    elseif isstruct(obj.data.trials(1).(Args.EventTiming))
+        for t = 1:length(obj.data.trials)
+          if ~isempty(obj.data.trials(t).(Args.EventTiming))
+            ts(t) = obj.data.trials(t).(Args.EventTiming).onset;
+          end
+        end
+    else
+        for t = 1:length(obj.data.trials)
+          if ~isempty(obj.data.trials(t).(Args.EventTiming))
+            ts(t) = obj.data.trials(t).(Args.EventTiming);
+          end
+        end
+      end
+  end
+  r = ts;
 else
 	% if we don't recognize and of the options, pass the call to parent
 	% in case it is to get number of events, which has to go all the way

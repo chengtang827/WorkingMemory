8b5c33b1c0a0650b5f1b65738060ac5e7c706f3d
roger <roger.herikstad@gmail.com>
2017-09-12 17:02:31 +0800

    Adds code to plot reaction time analysis from eyetrials.

--- @eyetrials/plot.m
+++ @eyetrials/plot.m
@@ -4,8 +4,8 @@ function [obj, varargout] = plot(obj,varargin)
 %   response.
 
 Args = struct('LabelsOff',0,'GroupPlots',1,'GroupPlotIndex',1,'Color','b', ...
-		  'ReturnVars',{''}, 'ArgsOnly',0,'TrialLevel',0);
-Args.flags = {'LabelsOff','ArgsOnly','TrialLevel'};
+		  'ReturnVars',{''}, 'ArgsOnly',0,'TrialLevel',0,'ReactionTime',0);
+Args.flags = {'LabelsOff','ArgsOnly','TrialLevel','ReactionTime'};
 [Args,varargin2] = getOptArgs(varargin,Args);
 
 % if user select 'ArgsOnly', return only Args structure for an empty object
@@ -44,7 +44,62 @@ end
 %create perceptually distinguisable colors for plotting
 plot_colors = distinguishable_colors(length(ff));
 
-if ~Args.TrialLevel
+if Args.TrialLevel
+	%plot a single trial
+	ax1 = subplot(1, 1, 1);
+	cla(ax1);
+	hold(ax1, 'on')
+	plot(ax1, obj.data.trials(tidx).gazex)
+	plot(ax1, obj.data.trials(tidx).gazey)
+	%plot triggers
+	t0 = obj.data.trials(tidx).start;
+	k = 3; %keep track of legends
+	legends = {'gazex', 'gazey'};
+	for fi=1:length(fidx)
+		if strcmpi(ff{fidx(fi)},'saccade')
+			continue %ksip saccade
+		end
+		if ~isempty(obj.data.trials(tidx).(ff{fidx(fi)}))
+			xx = obj.data.trials(tidx).(ff{fidx(fi)});
+			if isstruct(xx)
+				t = double(xx.onset - t0);
+			else
+				t = double(xx-t0);
+			end
+			plot(ax1, [t t], ylim, 'color',plot_colors(fi,:),...
+				   'linewidth', 2.0)
+			legends{k} = strrep(ff{fidx(fi)}, '_','');
+			k = k + 1;
+		end
+	end
+	legend(ax1, legends)
+	hold(ax1, 'off')
+elseif Args.ReactionTime
+	%get the session directory
+	nd = nptdata(obj);
+	session_dir = nd.SessionDirs{n};
+	%get experimental results
+	cwd = pwd;
+	cd(session_dir);
+	dlist = nptDir('*_results.txt');
+	if length(dlist) >= 1
+		dd = readtable(dlist(1).name);
+		delta_t = dd.response_on - dd.target_on;
+		%get only correct trials
+		idx = find((dd.response_on > 0)&(dd.reward_on > 0));
+		saccade_time = get(obj, 'EventTiming', 'saccade');
+		target_time = get(obj, 'EventTiming', 'target');
+		scatter(delta_t(idx), (saccade_time(idx) - target_time(idx))/1000);
+		hold on
+		mii = min(delta_t(idx));
+		mx = max(delta_t(idx));
+		plot([mii mx], [mii mx]);
+		xlabel('Response cue time [s]')
+		ylabel('Reaction time [s]')
+		hold off
+	end
+	cd(cwd);
+else
 	ydata = nan(ntrials,length(fidx));
 	for t = 1:ntrials
 		t0 = obj.data.trials(tidx(t)).start;
@@ -96,37 +151,6 @@ if ~Args.TrialLevel
 	ylim(ax2, [0 obj.data.screen_size(2)]);
 	hold(ax2, 'off')
 	%
-else
-	%plot a single trial
-	ax1 = subplot(1, 1, 1);
-	cla(ax1);
-	hold(ax1, 'on')
-	plot(ax1, obj.data.trials(tidx).gazex)
-	plot(ax1, obj.data.trials(tidx).gazey)
-	%plot triggers
-	t0 = obj.data.trials(tidx).start;
-	k = 3; %keep track of legends
-	legends = {'gazex', 'gazey'};
-	for fi=1:length(fidx)
-		if strcmpi(ff{fidx(fi)},'saccade')
-			continue %ksip saccade
-		end
-		if ~isempty(obj.data.trials(tidx).(ff{fidx(fi)}))
-			xx = obj.data.trials(tidx).(ff{fidx(fi)});
-			if isstruct(xx)
-				t = double(xx.onset - t0);
-			else
-				t = double(xx-t0);
-			end
-			plot(ax1, [t t], ylim, 'color',plot_colors(fi,:),...
-				   'linewidth', 2.0)
-			legends{k} = strrep(ff{fidx(fi)}, '_','');
-			k = k + 1;
-		end
-	end
-
-	legend(ax1, legends)
-	hold(ax1, 'off')
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 

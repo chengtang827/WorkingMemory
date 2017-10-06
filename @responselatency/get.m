2e115b84fdcf4212c657625c5f4b0466879d3f85
roger <roger.herikstad@gmail.com>
2017-10-06 10:12:55 +0800

    Adds code to get reaction time dependence from response latency objects.

--- @responselatency/get.m
+++ @responselatency/get.m
@@ -9,8 +9,10 @@ function [r,varargout] = get(obj,varargin)
 %
 %   Dependencies:
 
-Args = struct('ObjectLevel',0, 'AnalysisLevel',0,'TrialLevel',0, 'Smoothed',0);
-Args.flags ={'ObjectLevel','AnalysisLevel','TrialLevel'};
+Args = struct('ObjectLevel',0, 'AnalysisLevel',0,'TrialLevel',0, 'Smoothed',0,...
+							'ReactionTimeDependence',0,'glmfit',0);
+Args.flags ={'ObjectLevel','AnalysisLevel','TrialLevel','ReactionTimeDependence',...
+							'glmfit'};
 Args = getOptArgs(varargin,Args);
 
 % set variables to default
@@ -30,6 +32,55 @@ elseif Args.Smoothed > 0
 		scounts(:,i) = smooth(obj.data.counts(:,i),Args.Smoothed);
 	end
 	r = scounts;
+elseif Args.ReactionTimeDependence
+	nd = nptdata(obj);
+	celldir = nd.SessionDirs{1};
+	session_dir = getDataOrder('session','DirString',celldir);
+	cwd = pwd;
+	cd(session_dir)
+	et = eyetrials('auto');
+	cd(cwd)
+	rtime = get(et, 'ReactionTime');
+	gidx = intersect(find(~isnan(rtime)),obj.data.trialidx);
+	qidx = find(ismember(obj.data.trialidx, gidx));
+	scounts = zeros(size(obj.data.counts,1), length(gidx));
+	rtime = rtime(gidx);
+	w = 20;  % TODO: make this an argument
+	for i = 1:length(gidx)
+		scounts(:,i) = smooth(obj.data.counts(:,qidx(i)),w);
+	end
+	pvals = zeros(size(scounts,1),1);
+	mm = median(rtime);
+	upper_idx = find(rtime > mm);
+	lower_idx = find(rtime < mm);
+	for i = 1:length(pvals)
+		pvals(i) = ranksum(scounts(i,lower_idx), scounts(i, upper_idx));
+	end
+	vidx = find(pvals < 0.05);
+	%figure out the boundaries
+	qidx = find(diff(vidx) > 1);
+	%figure out where each region starts
+	stidx = [1;(qidx+1)];
+	%figure out where each region ends
+	eidx = [qidx;length(vidx)];
+	r = obj.data.xi([vidx(stidx) vidx(eidx)]);
+elseif Args.glmfit
+	window = [-0.15, 0.05];
+	widx = find((obj.data.xi < window(2))&(obj.data.xi > window(1)));
+	X = get(obj, 'Smoothed', 20);
+	y = sum(X(widx, :),1)';
+	nd = nptdata(obj);
+	session_dir = getDataOrder('session', 'DirString', nd.SessionDirs{1});
+	cwd = pwd;
+	cd(session_dir);
+	tr = trialsOld('auto');
+	et = eyetrials('auto');
+	cd(cwd);
+	tlabel = get(tr, 'TargetLabel');
+	rtime = get(et, 'ReactionTime');
+	X = [rtime(obj.data.trialidx,:) tlabel(obj.data.trialidx,:)];
+	m = fitglm(X, y, 'interactions', 'CategoricalVars',[2],'Distribution','poisson');
+	r = m;
 else
 	% if we don't recognize and of the options, pass the call to parent
 	% in case it is to get number of events, which has to go all the way
